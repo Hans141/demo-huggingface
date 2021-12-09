874e121245c1f0320d09c4973a6b185c79e31976
Hans141 <hans@techainer.com>
2021-12-09 17:21:18 +0700

    check

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from transformers import pipeline
 pipe = pipeline('sentiment-analysis')
-text = st.text_area('enter some text haha!')
+text = st.text_area('enter some text!')
 if text:
   out = pipe(text)
   st.json(out)
+
